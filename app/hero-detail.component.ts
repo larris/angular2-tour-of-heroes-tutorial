272ac13da896cb2149b720359209472eac597e51
lazaros fantidis <larris@lazaross-MacBook-Air.local>
2016-03-14 22:30:37 +0200

    first commit

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,30 @@
+import {Hero} from './hero';
+import {RouteParams} from 'angular2/router';
+import { HeroService } from './hero.service';
+import { Component, OnInit } from 'angular2/core';
+
+@Component({
+  selector: 'my-hero-detail',
+  templateUrl: 'app/hero-detail.component.html',
+
+})
+
+export class HeroDetailComponent {
+constructor(
+  private _heroService: HeroService,
+  private _routeParams: RouteParams) {
+}
+
+  hero:Hero;
+
+ngOnInit() {
+  let id = +this._routeParams.get('id');
+  this._heroService.getHero(id)
+    .then(hero => this.hero = hero);
+}
+goBack() {
+  window.history.back();
+}
+
+
+}
